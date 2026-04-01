fb1a4b54a83d8f969bab98988487968bd7fb3bf9
flo soc <florent.socrate@hotmail.fr>
2026-04-01 21:22:15 +0000

    rename column on staging, updated test column name add fact_answers and dim_users models add tests

--- dbt_dataflashcards/models/staging/stg_raw_data_dataflashcards__analytics_promo_raw.sql
+++ dbt_dataflashcards/models/staging/stg_raw_data_dataflashcards__analytics_promo_raw.sql
@@ -9,19 +9,18 @@ source as (
 renamed as (
 
     select
+        pseudo AS user_id,
+        document_id AS answer_id,
+        questionid AS question_id,
+        categorie AS category,
         resultat,
-        document_id,
-        tempsreflexion,
-        date,
-        etatauclic,
-        pseudo,
-        maitriseinstinctive,
+        CAST(CASE WHEN resultat = 'réussi' THEN true ELSE false END AS BOOL) AS is_pass, -- cast resultat colum string to bool -- add a test 'accepted_value' on source.
+        maitriseinstinctive AS is_mastered, 
+        tempsreflexion AS reflexion_time,
+        etatauclic AS state_impression,
+        date AS answer_date, -- format timestamp
         device,
-        ingested_at,
-        categorie,
         browser,
-        questionid
-
     from source
 
 )
--- dbt_dataflashcards/models/staging/stg_seed__raw_questions.sql
+++ dbt_dataflashcards/models/staging/stg_seed__raw_questions.sql
@@ -4,13 +4,15 @@ with source as (
 
 renamed as (
     select
-        id_unique,
-        id_radical,
-        cast(version as int) as version_number,
+        id_unique AS question_id,
+        id_radical AS base_id,
+        cast(version as int) as version,
         categorie as category,
-        recto as question_front,
-        verso as answer_back,
-        cast(date_creation as date) as created_at
+        recto as question_text,
+        LENGTH(recto) AS question_length,
+        verso as answer_text,
+        LENGTH(verso) AS answer_length,
+        CAST(date_creation as date) as created_at
     from source
     where id_unique is not null -- Nettoyage des lignes vides que tu as détectées
 )
